443e0187eb082368f888585623a333af70124a77
bastio <bastianelli.mic@gmail.com>
2025-12-07 19:26:43 +0100

    initial commit

---
+++ src/test/java/it/michele/watchlog/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package it.michele.watchlog;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
